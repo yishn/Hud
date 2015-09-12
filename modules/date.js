99cae9ca5da36d0c98bb00e60467b8ce5a06714d
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-12 20:56:13 +0200

    Update date module

--- modules/date.js
+++ modules/date.js
@@ -1,6 +1,8 @@
 var element
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
+var days = ['Sunday', 'Monday', 'Tuesday',
+    'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'November', 'December']
 
 exports.init = function(el) {
     element = el
@@ -12,8 +14,11 @@ exports.init = function(el) {
 exports.update = function() {
     var date = new Date()
     var day = date.getDate()
-    var th = day >= 10 && day < 20 ? 'th' :
-        (day % 10 == 1 ? 'st' : (day % 10 == 2 ? 'nd' : (day % 10 == 3 ? 'rd' : 'th')))
+    var th = 'th'
+
+    if (day < 10 || day >= 20) {
+        th = day % 10 == 1 ? 'st' : (day % 10 == 2 ? 'nd' : (day % 10 == 3 ? 'rd' : 'th'))
+    }
 
     element.text(days[date.getDay()] + ', ' + day + th + ' ' + months[date.getMonth()])
 }
