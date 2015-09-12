5ddfdbf09692473e1017f2a28b56523a9fd20c1d
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-12 19:51:41 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+var window = null
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() { app.quit() })
+
+app.on('ready', function() {
+    window = new BrowserWindow({
+        'resizable': false,
+        'skip-taskbar': true,
+        'show': false,
+        'frame': false,
+        'transparent': true
+    })
+
+    // window.toggleDevTools()
+
+    window.on('closed', function() { window = null })
+    window.webContents.on('did-finish-load', function() { window.show() })
+    window.webContents.on('will-navigate', function(e) { e.preventDefault() })
+
+    window.loadUrl('file://' + __dirname + '/index.html')
+})
