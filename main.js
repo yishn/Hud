38f1411b2b23a276adf00c89fabbaf10d6eea03e
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-13 14:27:15 +0200

    Enable F5 reload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.on('ready', function() {
     // window.toggleDevTools()
 
     window.on('closed', function() { window = null })
-    window.webContents.on('will-navigate', function(e) { e.preventDefault() })
+    // window.webContents.on('will-navigate', function(e) { e.preventDefault() })
 
     window.loadUrl('file://' + __dirname + '/index.html')
 })
