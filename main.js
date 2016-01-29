b6f2e6964d903da233287b3203fc941188bb835a
Yichuan Shen <shen.yichuan@gmail.com>
2016-01-29 22:44:26 +0100

    Update Electron

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ app.on('window-all-closed', function() { app.quit() })
 
 app.on('ready', function() {
     window = new BrowserWindow({
-        'width': 1000,
-        'height': 1000,
-        'resizable': false,
-        'skip-taskbar': true,
-        'show': false,
-        'frame': false,
-        'transparent': true
+        width: 1000,
+        height: 1000,
+        resizable: false,
+        skipTaskbar: true,
+        show: false,
+        frame: false,
+        transparent: true
     })
 
     // window.toggleDevTools()
