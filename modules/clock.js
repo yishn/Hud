35b17d62823c8bac1a8bc839d244d901d6430d58
Yichuan Shen <shen.yichuan@gmail.com>
2016-09-18 15:58:26 +0200

    Clock and date modules are more lenient on CPU

--- modules/clock.js
+++ modules/clock.js
@@ -1,16 +1,24 @@
+let cache = ''
+
 exports.init = function(el, settings) {
     exports.element = el
     exports.settings = settings
 
     exports.update()
-    setInterval(exports.update, 1000)
 }
 
 exports.update = function() {
     let date = new Date()
     let hours = date.getHours()
     let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
     minutes = minutes < 10 ? '0' + minutes : minutes
 
-    exports.element.text(hours + ':' + minutes)
+    let text = hours + ':' + minutes
+
+    if (text == cache) setTimeout(exports.update, 1000)
+    else setTimeout(exports.update, (60 - seconds) * 1000)
+
+    exports.element.text(text)
+    cache = text
 }
--- modules/date.js
+++ modules/date.js
@@ -3,22 +3,31 @@ const days = ['Sunday', 'Monday', 'Tuesday',
 const months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December']
 
+let cache = ''
+
 exports.init = function(el, settings) {
     exports.element = el
     exports.settings = settings
 
     exports.update()
-    setInterval(exports.update, 1000)
 }
 
 exports.update = function() {
     let date = new Date()
     let day = date.getDate()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
     let th = 'th'
 
     if (day < 10 || day >= 20) {
         th = day % 10 == 1 ? 'st' : (day % 10 == 2 ? 'nd' : (day % 10 == 3 ? 'rd' : 'th'))
     }
 
-    exports.element.text(days[date.getDay()] + ', ' + day + th + ' ' + months[date.getMonth()])
+    let text = days[date.getDay()] + ', ' + day + th + ' ' + months[date.getMonth()]
+    if (text == cache) setTimeout(exports.update, 1000)
+    else setTimeout(exports.update, (24 * 60 * 60 - hours * 60 * 60 - minutes * 60 - seconds) * 1000)
+
+    exports.element.text(text)
+    cache = text
 }
