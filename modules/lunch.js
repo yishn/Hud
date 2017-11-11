932d3dc29412acd38ba5e2c77e6aa1b6b9b280b8
Yichuan Shen <shen.yichuan@gmail.com>
2017-11-11 17:32:57 +0100

    Display lunch items

--- modules/lunch.js
+++ modules/lunch.js
@@ -1,6 +1,7 @@
 const {h, Component} = require('preact')
-const request = require('request')
 const {JSDOM} = require('jsdom')
+
+const dayDuration = 24 * 60 * 60 * 1000
 
 module.exports = class LunchModule extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ module.exports = class LunchModule extends Component {
 
         this.state = {
             data: null,
-            show: false
+            show: true
         }
     }
 
@@ -17,6 +18,9 @@ module.exports = class LunchModule extends Component {
     }
 
     update() {
+        let date = new Date()
+        let day = (date.getDay() || 7) - 1
+
         let url = 'http://sap-lunch-menu.appspot.com'
 
         JSDOM.fromURL(url).then(({window: {document: dom}}) => {
@@ -24,22 +28,41 @@ module.exports = class LunchModule extends Component {
                 'body', 'table', 'tbody', 'tr:nth-child(1)', 'td.Stage', 
                 'table', 'tbody', 'tr:nth-child(6)', 'td', 
                 'table', 'tbody', 'tr', 'td', 'div:nth-child(1)', 'p'
-            ].join('>'))].filter(p => p.textContent.trim() !== '').map(p => ({
-                date: p.querySelector('b').textContent.trim(),
+            ].join('>'))].filter(p => p.textContent.trim() !== '').map((p, i) => ({
+                date: new Date(date.getTime() + (i - day) * dayDuration),
                 content: [...p.querySelectorAll('table tr')].map(tr => ({
                     category: tr.querySelector('td:first-child').textContent
                         .trim().slice(0, -1),
                     value: tr.querySelector('td:last-child').textContent
-                        .replace(/\([^()]*\)/g, '').trim()
-                }))
+                        .replace(/\s*\([^)]*\)/g, '').trim()
+                })).filter(item => item.category.indexOf('Hauptgang') === 0)
             }))
 
             this.setState({data: {items}})
         })
     }
 
-    render(_, {data, show}) {
-        console.log(data)
-        return h('li', {id: 'lunch', class: data != null && show && 'show'})
+    render({maxlength}, {data, show}) {
+        let date = new Date()
+        let lunch = null
+
+        if (data != null) {
+            lunch = data.items.find(x => x.date.toDateString() === date.toDateString())
+            // lunch = data.items[0]
+        }
+
+        return h('li', {id: 'lunch', class: lunch != null && show && 'show'},
+            lunch != null && lunch.content.map((item, i) => [
+                i > 0 && h('br'),
+
+                h('strong', {}, item.category.split(/\s+/).map(x =>
+                    isNaN(x) ? (x.length === 0 ? '' : x[0].toUpperCase()) : x
+                ).join('')), ' ',
+
+                item.value.length < maxlength 
+                ? item.value 
+                : item.value.slice(0, Math.max(maxlength - 3, 0)).trim() + '…'
+            ])
+        )
     }
 }
